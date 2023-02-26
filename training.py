136db4f1dcdfb651718879f47f49b9811536429b
lilian <lilian.lungu.77@mail.ru>
2023-02-26 19:10:27 +0200

    changed file

--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+list_card_url = []
 headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
 for count in range(1,8):
     sleep(3)
@@ -9,9 +10,16 @@ for count in range(1,8):
     soup = BeautifulSoup(response.text, 'lxml')
     datas = soup.findAll('div', class_='col-lg-4 col-md-6 mb-4')
     for data in datas:
-        name = data.find('h4', class_='card-title').text
-        price = data.find('h5').text
-        url_img = 'https://scrapingclub.com' + data.find('img',class_='card-img-top img-fluid').get('src')
-        print(name + price, url_img, sep='\n')
+        card_url = "https://scrapingclub.com" + data.find('a').get('href')
+        list_card_url.append(card_url)
+for card_url in list_card_url:
+    response = requests.get(card_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = soup.find('div', class_='card mt-4 my-4')
+    name  = data.find('h3', class_='card-title').text
+    price = data.find('h4').text
+    text = data.find('p', class_='card-text').text
+    url_img = "https://scrapingclub.com" + data.find('img', class_='card-img-top img-fluid').get('src')
+    print(name, price, text, url_img, sep='\n')
 
 
